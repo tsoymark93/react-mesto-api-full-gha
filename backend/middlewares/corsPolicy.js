b06458c66bd7a5a5d3299cb9d69ff0675d5a57ed
Mark Tsoy <tsoy.mark@MacBook-Air-Mark.local>
2023-06-30 17:16:59 +0600

    corsPolicy

---
+++ backend/middlewares/corsPolicy.js
@@ -0,0 +1,28 @@
+const allowedCors = [
+  'https://mesto.bezprobeloff.nomoredomains.icu',
+  'http://api.mesto.bezprobeloff.nomoredomains.icu',
+  'https://localhost:3000',
+  'http://localhost:3000',
+];
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+const corsPolicy = (req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  return next();
+};
+
+module.exports = { corsPolicy };
