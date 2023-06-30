964273acfd80560ded9a7325db22477ac0bbc4bb
Mark Tsoy <tsoy.mark@MacBook-Air-Mark.local>
2023-06-30 19:28:51 +0600

    ss

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const centralError = require('./middlewares/centralError');
 const NotFoundError = require('./errors/NotFoundError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 const { PATH = 'mongodb://127.0.0.1:27017/mestodb' } = process.env.PATH;
 const app = express();
 
