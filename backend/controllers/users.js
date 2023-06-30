ab50da332b46722ac57ec0e50c7e55cdc48243b7
Mark Tsoy <tsoy.mark@MacBook-Air-Mark.local>
2023-06-30 22:51:42 +0600

    env dev

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -5,8 +5,7 @@ const ConflictError = require('../errors/ConflictError');
 const ValidationError = require('../errors/ValidationError');
 const NotFoundError = require('../errors/NotFoundError');
 
-// const secretKey = process.env.SECRET_KEY; -- не могу иначе пройти автотесты
-
+const { NODE_ENV, JWT_SECRET } = process.env;
 // eslint-disable-next-line arrow-body-style
 const updateUserFields = (userId, updateFields) => {
   return User.findByIdAndUpdate(userId, updateFields, { new: true, runValidators: true })
@@ -76,9 +75,11 @@ module.exports.login = (req, res, next) => {
   return User
     .findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'secret-key', {
-        expiresIn: '7d',
-      });
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        { expiresIn: '7d' },
+      );
       res.send({ token });
     })
     .catch((err) => next(err));
