6713b2e63f171db9243b88b7ab516af06bde3a7a
Mark Tsoy <tsoy.mark@MacBook-Air-Mark.local>
2023-06-30 23:06:17 +0600

    env dev

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = process.env;
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
 // const secretKey = process.env.SECRET_KEY; -- не могу иначе пройти автотесты
@@ -13,7 +15,7 @@ module.exports = (req, res, next) => {
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
-    payload = jwt.verify(token, 'secret-key');
+    payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     return next(new UnauthorizedError('Необходима авторизация'));
   }
