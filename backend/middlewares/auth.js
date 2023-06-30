116dbcb3f9fab214eb3cf94e4a6139af3bf1b514
Mark Tsoy <tsoy.mark@MacBook-Air-Mark.local>
2023-06-30 21:06:29 +0600

    authToken1

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,27 +1,31 @@
 const jwt = require('jsonwebtoken');
-const UnauthorizedError = require('../errors/UnauthorizedError');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
-module.exports = (req, res, next) => {
-  // рекомендуем записывать JWT в httpOnly куку - вынимаем
-  const token = req.headers.authorization.replace('Bearer ', '');
+const UnauthorizedError = require('../errors/UnauthorizedError');
 
-  if (!token) {
-    next(new UnauthorizedError('Ошибка авторизации'));
-    return;
+const auth = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
 
+  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(
+      token,
+      NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+    );
   } catch (err) {
-    next(new UnauthorizedError('Ошибка авторизации'));
-    return;
+    next(new UnauthorizedError('Необходима авторизация'));
   }
 
-  req.user = payload; // записываем пейлоуд в объект запроса
+  req.user = payload;
 
-  next(); // пропускаем запрос дальше
+  return next();
 };
+
+module.exports = auth;
