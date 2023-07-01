9204ee634e1983752db8080f62b215ff1ee2b2a9
Mark Tsoy <tsoy.mark@MacBook-Air-Mark.local>
2023-07-01 11:30:58 +0600

    env back1

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -5,8 +5,6 @@ const ConflictError = require('../errors/ConflictError');
 const ValidationError = require('../errors/ValidationError');
 const NotFoundError = require('../errors/NotFoundError');
 
-const secretKey = process.env.SECRET_KEY;
-
 // eslint-disable-next-line arrow-body-style
 const updateUserFields = (userId, updateFields) => {
   return User.findByIdAndUpdate(userId, updateFields, { new: true, runValidators: true })
@@ -72,16 +70,20 @@ module.exports.updateAvatar = (req, res, next) => {
 
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
+  const { JWT_SECRET = 'dev-secret' } = process.env;
 
-  return User
-    .findUserByCredentials(email, password)
+  return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, secretKey, {
-        expiresIn: '7d',
-      });
-      res.send({ token });
+      const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
+      res.status()
+        .cookie('jwt', token, {
+          maxAge: 3600000,
+          httpOnly: true,
+          sameSite: 'None',
+          secure: true,
+        }).send({ token });
     })
-    .catch((err) => next(err));
+    .catch(next);
 };
 
 module.exports.getCurrentUser = (req, res, next) => {
